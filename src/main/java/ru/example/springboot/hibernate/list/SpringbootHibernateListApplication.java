179cd1324b8a43fa252a4020b49eb28563845ded
Mikhail Moskvin <mos-mv@mail.ru>
2025-08-14 17:34:01 +0300

    Для включения транзиктивности добавлена аннотация @EnableTransactionManagement

--- src/main/java/ru/example/springboot/hibernate/list/SpringbootHibernateListApplication.java
+++ src/main/java/ru/example/springboot/hibernate/list/SpringbootHibernateListApplication.java
@@ -2,8 +2,10 @@ package ru.example.springboot.hibernate.list;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootHibernateListApplication {
 
 	public static void main(String[] args) {
